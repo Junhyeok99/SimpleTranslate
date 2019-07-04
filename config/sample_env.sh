04452d4881884f43c0ef170d2bbd78c4e7a19787
Junhyeok99 <apple2870907@gmail.com>
2019-07-04 10:31:28 +0900

    fix minor details

---
+++ config/sample_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#export GOOGLE_APPLICATION_CREDENTIALS=your_Key_file_location.json
+export GOOGLE_APPLICATION_CREDENTIALS=sample.json
--- sample_env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export GOOGLE_APPLICATION_CREDENTIALS=your_Key_file_location.json
